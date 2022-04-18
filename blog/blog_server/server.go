1b2d069852ece4a1bee07ea87b02590aaf5db7f6
William H.W <wiliamwijaya1985@gmail.com>
2022-04-18 17:06:52 +0700

    feat(blog): create proto buffer and blog server

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/wiliamhw/golang-grpc-example/blog/blogpb"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+)
 
+type server struct {
+	blogpb.UnimplementedBlogServiceServer
+}
+
+const port = 50051
+
+func main() {
+	// Show the file name and line number of error.
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	fmt.Println("Blog Service Started")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	go func() {
+		fmt.Println("Starting Server...")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	// Wait for control C to exit
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	// Block until a signal is received
+	<-ch
+
+	fmt.Println("Stopping the server")
+	s.Stop()
+	fmt.Println("Closing the listener")
+	lis.Close()
+	fmt.Println("End of Program")
 }
--- generate.sh
+++ generate.sh
@@ -3,6 +3,11 @@
 protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
     greet/greetpb/greet.proto
+
 protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
     calculator/calculatorpb/calculator.proto
+
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    blog/blogpb/blog.proto
