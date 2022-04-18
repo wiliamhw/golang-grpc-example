26151dbbd270c1ec2704005fc6956fcf04a60020
William H.W <wiliamwijaya1985@gmail.com>
2022-04-18 18:08:17 +0700

    feat(blog): create client to consume readBlog function

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -28,16 +28,41 @@ func main() {
 
 	c := blogpb.NewBlogServiceClient(cc)
 
-	// Create blog
+	blog := createBlog(c)
+	readBlog(c, blog.GetId())
+}
+
+func createBlog(c blogpb.BlogServiceClient) *blogpb.Blog {
 	fmt.Println("Creating the blog")
 	blog := &blogpb.Blog{
 		AuthorId: "Stephane",
 		Title:    "My First Blog",
 		Content:  "Content of the first blog",
 	}
-	createBlogRes, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
+	res, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
 	if err != nil {
 		log.Fatalf("Unexpected error: %v", err)
 	}
-	fmt.Printf("Blog has been created: %v", createBlogRes)
+	fmt.Printf("Blog has been created: %v\n", res)
+	return res.GetBlog()
+}
+
+func readBlog(c blogpb.BlogServiceClient, blogId string) {
+	fmt.Println("Reading the blog")
+
+	requests := [...]*blogpb.ReadBlogRequest{
+		{
+			BlogId: "1dfsoijfs",
+		}, {
+			BlogId: blogId,
+		},
+	}
+
+	for _, req := range requests {
+		res, err := c.ReadBlog(context.Background(), req)
+		if err != nil {
+			fmt.Printf("Error happened while reading: %v", err)
+		}
+		fmt.Printf("Blog was read: %v\n", res)
+	}
 }
